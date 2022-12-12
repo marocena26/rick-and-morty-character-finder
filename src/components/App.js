2e16901eec294d90d187dd7e90cbbaf3f733a6c7
marocena26 <mariaelena.arocena@gmail.com>
2022-12-12 19:39:11 +0100

    add useEffect and  useState for paint characters

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
-import '../styles/App.scss';
+import { useEffect, useState } from "react";
+import getDataFromAPI from "../services/api.js";
+import "../styles/App.scss";
 
 function App() {
+  const [dataCharacter, setDataCharacter] = useState([]); //VARIABLE ESTADO para guardar los datos de la API
+
+  //En el ejecutare api.js
+  useEffect(() => {
+    getDataFromAPI().then((dataEachCharacter) => {
+      setDataCharacter(dataEachCharacter);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Hola mundo</h1>
