4d59d9b905c007ea8dc93efd6eb28b1b097c7ee0
marocena26 <mariaelena.arocena@gmail.com>
2022-12-13 19:41:31 +0100

    add specification to order A to Z

--- src/services/api.js
+++ src/services/api.js
@@ -16,6 +16,7 @@ const callToApi = () => {
           status: eachCharacter.status,
         };
       });
+      characters.sort((x, y) => x.name.localeCompare(y.name));
       return characters; //Este es mi nuevo objeto creado con los datos que me interesan. Lo tengo que retornar para que no se quede guardado en el fetch, de esta manera puedo utilizarlo ahora en App.
     });
 };
